929ca3c9ebb6e198ca19632634d6c612aebd5662
omer111-50 <omerali0446@gmail.com>
2023-10-28 19:59:01 +0100

    Feat: Completion of level 1

--- Src/script.js
+++ Src/script.js
@@ -1,2 +1,27 @@
-// Basic alert
-alert("Welcome to GUH 2023! This is a basic example of JavaScript alert.");
+const invisibleButton = document.getElementById("invisible-button");
+const flashingDiv = document.getElementById("flashing-div");
+const mouseTrail = document.getElementById("mouse-trail");
+
+invisibleButton.addEventListener("click", () => {
+  alert("You found the invisible button! Level 1 Complete!");
+
+  window.location.href = "level2.html";
+});
+
+document.addEventListener("mousemove", (e) => {
+  const trail = document.createElement("div");
+  trail.style.left = e.clientX - 10 + "px";
+  trail.style.top = e.clientY - 10 + "px";
+  mouseTrail.appendChild(trail);
+  setTimeout(() => {
+    mouseTrail.removeChild(trail);
+  }, 1000);
+});
+
+flashingDiv.addEventListener("mouseenter", () => {
+  flashingDiv.style.backgroundColor = "yellow";
+});
+
+flashingDiv.addEventListener("mouseleave", () => {
+  flashingDiv.style.backgroundColor = "red";
+});
